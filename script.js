e4814c64e4ddabe5f73a10318e4143b9040bcc3d
farhan <ff1917914@gmail.com>
2024-09-10 11:46:27 -0700

    Added more htmls

---
+++ script.js
@@ -0,0 +1,100 @@
+//  chapter no 1 (alert)
+
+
+    // Q1
+
+// alert("hello");
+
+// Q2
+
+// alert("Error! please enter a valid password");
+
+// Q3
+
+
+
+// alert("welcome to js land \n Happy coding");
+
+
+// Q4
+
+// alert("welcome to js  land ");
+
+// alert("happy coding ");
+
+// Q5
+
+
+// alert("hello i can run js through my web browser console");
+
+// Q6
+
+// alert("hello html and css ")
+
+
+// Q7
+
+// answer complete 
+
+
+// chapter no 2 (variable for number)
+
+// Q1
+
+// var age = "im 15 year old"
+
+// alert(age)
+
+// Q2
+
+// var time = "you have wsited this website for 15 times"
+
+// alert(time);
+
+// Q3
+
+// document.write("my birth year is 1990 <br> data type of my ddeclared variable is number")
+
+// Q4
+
+// document.write("jhone doe ordered 5 t-shirt (s) on XYZ clothing store")
+
+
+
+
+// chapter no 3 (variable name leggal and illegal)
+
+
+// Q1
+
+
+// var num1 = 123,num2 = 234,num3 = 345
+
+// alert(num1,num2,num3)
+
+// Q2
+
+// var 756name(5632654)
+// var 756name(674536)
+// var 756name(7543725435)
+// var 756name(6754733)
+// var 756name(73543745)
+
+
+// var name754(5632654)
+// var name754(674536)
+// var name754(7543725435)
+// var name754(6754733)
+// var name754(73543745)
+
+
+// Q3
+
+// samajh nahi araha 
+
+
+// assignment 1 complete /////
+
+
+
+
